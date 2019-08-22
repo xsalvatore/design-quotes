a13aef2dc6da89be7937310a6c6fff209901f263
Xavier Salvatore <xavier.salvatore@sogematech.com>
2019-08-22 12:04:18 -0400

    Fixed the URL for the API

--- src/components/App.js
+++ src/components/App.js
@@ -3,76 +3,93 @@ import axios from 'axios';
 import he from 'he';
 
 class App extends Component {
-
 	// holds the current state of the component
 	state = {
 		loading: true,
-		quote: ""
-	}
+		quote: ''
+	};
 
 	// performs the quote fetching when the components have been mounted
-	componentDidMount = () => {
+	componentDidMount = () => {
 		this.fetchQuote();
-	}
+	};
 
-	fetchQuote = () => {
+	fetchQuote = () => {
 		// holds the quote that will be fetched
-		let quote = "";
+		let quote = '';
 
 		// needed to get a different quotes on every fetch - 42 is the biggest number usable
 		let posts = Math.floor(Math.random() * 42);
-		
-		// fetches the data 
-		axios.get(`http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${posts}`)
-  			.then((response) => {
+
+		// fetches the data
+		axios
+			.get(
+				`https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${posts}`
+			)
+			.then((response) => {
 				// saves the fetched quote
 				quote = response.data[0].content;
-				
+
 				// removes the html tags at the beginning and end of the quote
 				quote = quote.substr(3).slice(0, -5);
-				
-				// converts the html entities to text and saves it 
+
+				// converts the html entities to text and saves it
 				quote = he.decode(quote);
 			})
-			.then(() => {
+			.then(() => {
 				// sets the state with the quote that was fetched
 				this.setState({
 					loading: false,
 					quote: quote
 				});
 			})
-  			.catch(function (error) {
+			.catch(function(error) {
 				// prints the error if there is any
-    			console.log(error);
+				console.log(error);
 			});
-		
-	}
+	};
 
 	// opens a new tab to send the quote as a tweet
-	tweetQuote = () => {
+	tweetQuote = () => {
 		// opens a new tab to tweet the quote the user had received
-		const win = window.open(`https://twitter.com/intent/tweet?text=${this.state.quote}`, "_blank");
+		const win = window.open(
+			`https://twitter.com/intent/tweet?text=${this.state.quote}`,
+			'_blank'
+		);
 
 		// changes the tab to the one with twitter
 		win.focus();
-	}
+	};
 
 	// renders the components
 	render() {
 		return (
-			<div className="app">
-				<div className="quote-wrapper">
+			<div className='app'>
+				<div className='quote-wrapper'>
 					<section>
 						<h1>Random Quotes Machine</h1>
-						<p className="quote">{this.state.loading ? "" : this.state.quote}</p>
+						<p className='quote'>
+							{this.state.loading ? '' : this.state.quote}
+						</p>
 					</section>
-					
+
 					<section>
 						<button onClick={this.fetchQuote}>Get Quote</button>
 						<button onClick={this.tweetQuote}>Tweet Quote</button>
 					</section>
 
-					<footer>Quotes provided by the <a href="https://quotesondesign.com/api-v4-0/" rel="noopener noreferrer" target="_blank"> quotesondesign </a> API.</footer>
+					<footer>
+						Quotes provided by the{' '}
+						<a
+							href='https://quotesondesign.com/api-v4-0/'
+							rel='noopener noreferrer'
+							target='_blank'
+						>
+							{' '}
+							quotesondesign{' '}
+						</a>{' '}
+						API.
+					</footer>
 				</div>
 			</div>
 		);
